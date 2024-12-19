8b1f28e93e8ec2932a4a099553d8da9aec0b95c5
Mohamed H. Amer <mohamed.hamdy.amer@gmail.com>
2024-12-19 15:51:17 +0000

    fixing 'images-config/jenkins/setupSecurity.groovy' ...

--- images-config/jenkins/setupSecurity.groovy
+++ images-config/jenkins/setupSecurity.groovy
@@ -6,28 +6,27 @@ import hudson.security.*
 def instance = Jenkins.get()
 
 def realm = instance.getSecurityRealm()
-
 def realmType = realm.getClass().toString()
-if (realmType == "class hudson.security.HudsonPrivateSecurityRealm") {
-    System.exit(0)
+
+if (realmType != "class hudson.security.HudsonPrivateSecurityRealm") {
+
+    realm = new HudsonPrivateSecurityRealm(false)
+
+    def username = "admin"
+    def password = "changeme"
+    def userDescription = "Admin Account"
+    def userEmail = "admin@cr.app"
+
+    def user = realm.createAccount(username, password)
+    user.setFullName(username)
+    user.setDescription(userDescription)
+    user.addProperty(new Mailer.UserProperty(userEmail))
+
+    instance.setSecurityRealm(realm)
+
+    def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+    strategy.setAllowAnonymousRead(false)
+    instance.setAuthorizationStrategy(strategy)
+
+    instance.save()
 }
-
-realm = new HudsonPrivateSecurityRealm(false)
-
-def username = "admin"
-def password = "changeme"
-def userDescription = "Admin Account"
-def userEmail = "admin@cr.app"
-
-def user = realm.createAccount(username, password)
-user.setFullName(username)
-user.setDescription(userDescription)
-user.addProperty(new Mailer.UserProperty(userEmail))
-
-instance.setSecurityRealm(realm)
-
-def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
-strategy.setAllowAnonymousRead(false)
-instance.setAuthorizationStrategy(strategy)
-
-instance.save()
