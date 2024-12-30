7cb8cdbb028b0df6f501cef09fd9a7ca0e1cf5f6
Mohamed H. Amer <mohamed.hamdy.amer@gmail.com>
2024-12-30 07:10:08 +0000

    renaming setUrl.groovy -> setURL.groovy ...

--- images-config/jenkins/setURL.groovy
+++ images-config/jenkins/setURL.groovy
@@ -1,7 +1,7 @@
 import jenkins.model.*
 
-def Jenkins_URL = System.getenv('Jenkins_URL')
+def jenkinsURL = System.getenv('Jenkins_URL')
 
 def jenkinsLocationConfiguration = JenkinsLocationConfiguration.get()
-jenkinsLocationConfiguration.setUrl(Jenkins_URL)
+jenkinsLocationConfiguration.setUrl(jenkinsURL)
 jenkinsLocationConfiguration.save()
